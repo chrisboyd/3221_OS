1e5e4c5e92a3fa3313b312d4ffbe4fe7cb29955c
Chrs Boyd <chrisboyd360@gmail.com>
2020-02-03 13:08:56 -0500

    Completed MiniProj1
    Everythings as required in Project_Desc.....
    
    One bug when using pipe to output cat file.txt | less when the file does not fill more that one screen system hangs waiting for less to finish? Pressing q or enter does not end less execution

--- MiniProj1_Shell/boyd-shell.c
+++ MiniProj1_Shell/boyd-shell.c
@@ -1,3 +1,26 @@
+/*
+ * Course: EECS 3221 Operating Systems
+ * Assigment: MiniProject 1
+ * Name: Christopher Boyd
+ * Student #: 216 869 356
+ * 
+ * Overview: 
+ * Implements a simple Linux shell.
+ * 
+ * Compile:
+ * make
+ * 
+ * Run:
+ * ./boyd-shell
+ * 
+ * Sample Usage:
+ * BoydShell$ ls -al > out.txt 		//redirect ls -al to file out.txt
+ * BoydShell$ cat file.txt | less 	//pipe output of cat to input of less
+ * BoydShell$ ls -al & 				//run ls -al in the background
+ * BoydShell$ sort < in.txt 		//use in.txt as input to sort command
+ * 
+ * 
+ */
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -8,196 +31,232 @@
 #include <stdbool.h>
 #include <fcntl.h> 
 
-
 #define MAX_LINE		80  //80 char per line
 
-bool isBackground(char **argv)
-{
+/*
+ * Function: is_background
+ * ---------------------------------
+ * checks array of input strings for the presence of the '&'
+ * character and returns true if it is present and sets the 
+ * value of the array index holding '&' to NULL
+ */
+bool is_background(char **args) {
 	bool bg = false;
-	while (*argv != NULL && !bg){
-		if (**argv == '&'){
+	while (*args != NULL && !bg) {
+		if (**args == '&') {
 			bg = true;
-			*argv = NULL;
-		} 
-		*argv++;
+			*args = NULL;
+		}
+		*args++;
 	}
 	return bg;
 }
 
-void  parse(char *line, char **argv)
-{
-	while (*line != '\0') {       /* if not the end of line ....... */ 
+/*
+ * Function: parse
+ * ---------------------------------
+ * NOTE: this is the code from 
+ * www.csl.mtu.edu/cs4411.ck/www/NOTES/process/fork/exec.html
+ * 
+ * I had my own parse function that but was running major issues with it. 
+ * In hindsight it was because I was using strtok. I ended up using strtok to 
+ * break up the major bits of text on white space but then had to edit the end of 
+ * each token to remove the newlines. Which ended up resulting in code that was
+ * virtually the same as this.
+ * 
+ * Replaces all white space characters in line with \0, then puts all words
+ * consisting of one or more letter into array pointed at by args * 
+ * 
+ */
+void parse(char *line, char **args) {
+	while (*line != '\0') { /* if not the end of line ....... */
 		while (*line == ' ' || *line == '\t' || *line == '\n')
-			*line++ = '\0';     /* replace white spaces with 0    */
-		*argv++ = line;          /* save the argument position     */
-		while (*line != '\0' && *line != ' ' && 
-			*line != '\t' && *line != '\n') 
-				line++;             /* skip the argument until ...    */
+			*line++ = '\0'; /* replace white spaces with 0    */
+		*args++ = line; /* save the argument position     */
+		while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
+			line++; /* skip the argument until ...    */
 	}
-	*argv = '\0';                 /* mark the end of argument list  */
+	*args = '\0'; /* mark the end of argument list  */
 }
-  
-void  execute(char **argv, char *file)
-{
-	pid_t  pid;
-	int    status;
-	bool bg = isBackground(argv);
+
+/*
+ * Function: execute
+ * ---------------------------------
+ * takes in an array of consisting of command followed by 
+ * arguments for said command and, optionally, a filename
+ * 
+ * Forks a child purpose within which said command is executed via
+ * execvp.
+ * 
+ * If a file name is passed in will open a file descriptor to the
+ * file for the purpose of reading / writing to / from, respectively.
+ * 
+ * If the array of args contains the '&' character, the child
+ * process will be run in the background. IE the parent process
+ * will not wait on the background process to finish before 
+ * continueing.c
+ */
+void execute(char **args, char **file) {
+	pid_t pid;
+	int status;
+	bool bg = is_background(args);
 	int fd;
-	
+
 	if ((pid = fork()) < 0) {     //fork a child process           
 		printf("*** ERROR: forking child process failed\n");
 		exit(1);
-	}
-	else if (pid == 0) {          // for the child process
-		if (file != NULL){
-			fd = open(file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	} else if (pid == 0) {          // for the child process
+		if (file != NULL) {
+			fd = open(*file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 			dup2(fd, 1);
 			close(fd);
-		}		         
-		if (execvp(*argv, argv) < 0) {     // execute the command 
+		}
+		if (execvp(*args, args) < 0) {     // execute the command 
 			printf("*** ERROR: exec failed\n");
 			exit(1);
 		}
-	}
-	else {                                  // for the parent:     
+	} else {                                  // for the parent:     
 		if (!bg)
-			while (wait(&status) != pid);      // wait for completion 
-		fflush(stdout);
+			while (wait(&status) != pid)
+				;      // wait for completion 
+		fflush (stdout);
 	}
 }
 
-int getFD(char *filename)
-{
-	int file_desc;
-	if (file_desc = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR) < 0){
-		printf("open failed: %s\n", filename);
-		exit(1);
-	}
-	
-	return file_desc;
-}
-
-void executePipe(char **outputcmd, char **inputcmd)
-{
-	pid_t  pid1, pid2, wpid;
+/*
+ * Function: execute_pipe
+ * ---------------------------------
+ * uses inter-process pipe to output the results from 
+ * using execvp of outputcmd as the input to inputcmd execvp
+ * 
+ */
+void execute_pipe(char **outputcmd, char **inputcmd) {
+	pid_t pid1, pid2, wpid;
 	int fds[2], status;
 	pipe(fds);
-	
+
 	if ((pid1 = fork()) < 0) {     //fork a child process           
 		printf("*** ERROR: forking child process failed\n");
 		exit(1);
-	}
-	else if (pid1 == 0) {          // for the child process
+	} else if (pid1 == 0) {          // for the child process
 		//open up write on this end of the pipe
-		dup2(fds[0], 0);  
-		
-		 
+		dup2(fds[0], 0);
+
 		if (execvp(*inputcmd, inputcmd) < 0) {     // execute the command 
 			printf("*** ERROR: exec failed\n");
 			exit(1);
 		}
-	}
-	else if ((pid2 = fork()) < 0) {
+	} else if ((pid2 = fork()) < 0) {
 		printf("*** ERROR: forking second child process failed\n");
 		exit(1);
-	}
-	else if (pid2 == 0){
+	} else if (pid2 == 0) {
+		//open read side of pipe
 		dup2(fds[1], 1);
-		
+
 		if (execvp(*outputcmd, outputcmd) < 0) {     // execute the command 
 			printf("*** ERROR: exec failed\n");
 			exit(1);
 		}
-	}
-	else {                                      
-		while ((wpid = wait(&status)) > 0);
-	    close(fds[0]);
-	    close(fds[1]);
+	} else {
+		while ((wpid = wait(&status)) > 0)
+			;
+		close(fds[0]);
+		close(fds[1]);
 	}
 }
- 
-void  main(void)
-{
-	char line[MAX_LINE];             
-	char *argv[MAX_LINE/2 + 1];
-	char *inputcmd[MAX_LINE/2 + 1];
-	char *outputcmd[MAX_LINE/2 + 1];
-	char *trimmed[MAX_LINE/2 +1];
+
+/*
+ * Function: split
+ * ---------------------------------
+ * Given an input string, line, splits the line into two arrays of 
+ * strings based on delimiter. Left being the array of strings that 
+ * occur to the left and right the right. 
+ * 
+ * IE: left delimiter right
+ * 
+ * Last element of both left and right is marked NULL
+ * 
+ */
+void split(char *line, char **left, char **right, char *delimiter) {
+	char *trimmed[MAX_LINE / 2 + 1];
+	int i = 0, j = 0;
+
+	parse(line, trimmed);
+
+	while (strcmp(trimmed[i], delimiter) != 0) {
+		left[i] = trimmed[i];
+		i++;
+	}
+	left[i] = NULL;
+	i++;
+	while (trimmed[i] != NULL) {
+		right[j] = trimmed[i];
+		i++;
+		j++;
+	}
+	right[j] = NULL;
+}
+
+/*
+ * Function: main
+ * ---------------------------------
+ * Processes user input and determines which command is
+ * entered. Stores last command executed.
+ * 
+ * Will run until user types 'exit'
+ */
+void main(void) {
+	char line[MAX_LINE];
+	char *args[MAX_LINE / 2 + 1];
+	char *source[MAX_LINE / 2 + 1];
+	char *dest[MAX_LINE / 2 + 1];
 	char lastcmd[MAX_LINE] = "";
-	char *source;
-	char *dest;
-	int fd = 0;
-	
-	while (1) {                  
-		printf("BoydShell -> ");     
-		fflush(stdout);
-		source = NULL;
-		dest = NULL;
-		//read command and remove new line
+
+	while (1) {
+		printf("BoydShell$ ");
+		fflush (stdout);
+
+		//read input command and remove new line
 		fgets(line, MAX_LINE, stdin);
 		printf("\n");
 		if (line[strlen(line) - 1] == '\n')
 			line[strlen(line) - 1] = '\0';
-		
-		
-		if (strcmp(line, "!!") == 0){
-			if (strcmp(lastcmd, "") == 0){
+
+		//check for history command
+		if (strcmp(line, "!!") == 0) {
+			if (strcmp(lastcmd, "") == 0) {
 				printf("No commands in history\n");
 				strcpy(line, "");
-			}
-			else{
+			} else {
 				printf("Last command: %s being executed\n\n", lastcmd);
 				strcpy(line, lastcmd);
 			}
 		}
+		//keep record of last command
 		else
 			strcpy(lastcmd, line);
-		
-		//need to parse strings returned by strtok to remove whitespace
-		//Potential cleanup: parse line before strtok, then remove index
-		//with < or >
-		if( strchr(line, '>') != NULL || strchr(line, '<') != NULL ){
-			source = strtok(line, "<>");
-			dest = strtok(NULL, "<>");
-			//remove trailing whitespace from source, can't use parse as
-			//that breaks things like "ls -al", put into line for 
-			//processing into argv
-			source[strlen(source) - 1] = '\0';
-			
-			//remove leading and trailing whitespace after strtok				
-			parse(dest, trimmed);
-			strcpy(dest, trimmed[0]);
-			
-			parse(source, argv); 
-			execute(argv, dest); 			
+
+		//process potential input formats
+		if (strchr(line, '>') != NULL) {
+			split(line, source, dest, ">");
+			execute(source, dest);
+		} else if (strchr(line, '<') != NULL) {
+			split(line, source, dest, "<");
+			execute(source, dest);
+
+		} else if (strchr(line, '|') != NULL) {
+			split(line, source, dest, "|");
+			execute_pipe(source, dest);
 		}
-		else if( strchr(line, '|') != NULL ){
-			parse(line, trimmed);
-			int i = 0, j = 0;
-			
-			while (strcmp(trimmed[i], "|") != 0){
-				outputcmd[i] = trimmed[i];
-				i++;
-			}
-			outputcmd[i] = NULL;
-			i++;
-			while (trimmed[i] != NULL){
-				inputcmd[j] = trimmed[i];
-				i++;
-				j++;
-			}
-			inputcmd[j] = NULL;
-			executePipe(outputcmd, inputcmd);
-		}
-		else if (strcmp(line, "") != 0){
+		//no special redirect characters in input, 
+		//process basic command
+		else if (strcmp(line, "") != 0) {
 			//parse input and execute command
-			parse(line, argv); 
-	
-			if (strcmp(argv[0], "exit") == 0)	
-				exit(0);  
-			execute(argv, dest); 
-		}  		
+			parse(line, args);
+			if (strcmp(args[0], "exit") == 0)
+				exit(0);
+			execute(args, NULL);
+		}
 	}
 }
 
-                
