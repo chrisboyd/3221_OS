6916b4f460ff40e4b72b3b0ef1cbac2cb554e145
Chrs Boyd <chrisboyd360@gmail.com>
2020-03-21 22:01:32 -0400

    can read from addresses
    have to convert read addresses to binary, mask for offset and page number and convert both back to decimal

--- MiniProj3_MMU/StartKit/mmu.c
+++ MiniProj3_MMU/StartKit/mmu.c
@@ -10,28 +10,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "mmu.h"
+
+unsigned int mask = 11111111;
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2){
+	if (argc < 3){
 		printf("Missing Argument:\n");
 		printf("Usage: ./mmu file.bin addresses.txt\n");
 		exit(1);
 	}
-	else{
-		printf("right args\n");
+	
+    FILE *log_addr;
+    log_addr = fopen(argv[2],"r");
+   	int addr = 0;
+	int j = 0;
+	
+	int offset = 0;
+	int page = 0;
+	
+	while (j < 10){
+		fscanf(log_addr, "%d", &addr);
+		//char *bin = int2bin(addr);
+		printf("%d -> %s\n", addr, bin);
+		
+		j++;
 	}
-	/*
-    FILE *in;
-    in = fopen(argv[1],"r");
-    
-    while (fgets(task,SIZE,in) != NULL) {
-       
-        free(temp);
-    }
 
-    fclose(in);
-*/
+    fclose(log_addr);
+
 
     return 0;
 }
+
+int bin2dec(int n){
+	int temp = n;
+	int dec = 0;
+	int base = 1;
+}
+int dec2bin(int n){
+	
+}
+/*
+char *int2bin(int n) {
+    int nbits = sizeof(n) * 8;
+    char *s = malloc(nbits+1);  
+    s[nbits] = '\0';
+    unsigned int u = *(unsigned int*)&n;
+    int i;
+    unsigned int mask = 1 << (nbits-1); 
+	// fill in values right-to-left
+    for (i = 0; i < nbits; i++, mask >>= 1)
+        s[i] = ((u & mask) != 0) + '0';
+    return s;
+}
+*
+
+
---
+++ MiniProj3_MMU/StartKit/mmu.h
@@ -0,0 +1,3 @@
+//char *int2bin(int n);
+int bin2dec(int n);
+int dec2bin(int n);
